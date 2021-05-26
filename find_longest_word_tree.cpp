225eae08040a3436b5d57ab7b55224a0830b52e8
Tim Ricker <timr@tkrf.net>
2021-05-25 23:41:21 -0600

    cleaned up some code

--- find_longest_word_tree.cpp
+++ find_longest_word_tree.cpp
@@ -1,5 +1,6 @@
 //
 // g++  -Wall -O3 -std=c++20 -O3 -o find_longest_word_tree find_longest_word_tree.cpp
+//
 #include <iostream>
 #include <string>
 #include <iomanip>
@@ -31,44 +32,34 @@ struct Node {
     NodePtr parent;
 };
 
-inline auto newNode(const char key) -> Node*
+inline auto newNode(const char key) -> NodePtr
 {
-    auto temp = make_shared<Node*> (new Node);
+    auto temp = make_shared<NodePtr> (new Node);
     (*temp)->key = key;
     return *temp;
 }
 
-//struct {
-//    inline bool operator() (const string & i, const string & j) { return (i.length() < j.length());}
-//} lessthan;
-struct {
-    inline bool operator() (const string & i, const string & j) { return (i.length() > j.length());}
-} greaterthan;
-
 inline static Node * root{nullptr};
 
-//auto mySort = [](const string & lhs, const string & rhs) {
-//             return lhs.length() == rhs.length() ?
-//                    lhs > rhs : lhs.length() > rhs.length(); };
+inline auto greaterthan = [](const string & i, const string & j) { return (i.length() > j.length());};
 
-inline constexpr auto setRoot(Node * _root) -> void
+inline constexpr auto setRoot(NodePtr _root) -> void
 {
     root = _root;
 }
 
-inline auto getRoot() -> Node*
+inline auto getRoot() -> NodePtr
 {
     return root;
 }
 
-//struct {
-//    bool operator() (Node * node, char key) { return node->key == key; }
-//} myfind;
 // Pass key in val, root or parent node of key node, on success a return true and retNode has its node.
-inline auto find_char_node (char key, NodePtr node, NodePtr & retNode) -> bool
+inline auto find_char_node (const char key, const NodePtr node, NodePtr & retNode) -> bool
 {
-    for  (auto iter : node->child) {
-        if (key == iter->key) {
+    retNode = nullptr;
+    for  (auto & iter : node->child) {
+        auto val = (*iter).key;
+        if (key == val) {
             retNode = iter;
             return true;
         }
@@ -78,29 +69,25 @@ inline auto find_char_node (char key, NodePtr node, NodePtr & retNode) -> bool
 }
 
 inline auto isWord (
-                string word, 
-                Node * node, 
-                Node * & retNode
+                const string word, 
+                NodePtr node,
+                NodePtr & retNode
                 ) -> bool
 {
     if (node == nullptr) {
         return false;
     }
 
-    size_t len = word.length();
-    if (len == 0) {
+    if (word.length() == 0) {
         return false;
     }
 
-    string wordstr;
     retNode = node;
 
-    wordstr.clear();
     bool retval {false};
-    for (auto letter : word) {
+    for (auto & letter : word) {
         retval = find_char_node (letter, node, retNode);
         if (retval) {
-//            wordstr += letter;
             node = retNode;
         } else {
             return false;       // letter in word not there
@@ -116,76 +103,67 @@ inline auto isWord (
 }
 
 inline auto isCompoundWord(
-                string word,
+                const string word,
                 NodePtr node,
                 NodePtr & retNode,
                 StringVector & retVector
-                ) -> bool
+                ) -> void
 {
-    if (node == nullptr) {
-        return false;
-    }
-
-    //cout << "searching for all words in tree: [" << word << "] " << endl;
-    size_t len = word.length();
-    if (len == 0) {
-        return false;
-    }
-
-    string wordstr;
-    retNode = node;
-
-    retVector.clear();
-    //bool retval {false};
-    for (auto letter : word) {
-        bool retval = find_char_node (letter, node, retNode);
-        if (retval) {
-            wordstr += letter;
-            if (retNode->EOW || wordstr.length() == len - 1) {
-                if (word.length() != wordstr.length()) { 
-                    retVector.emplace_back(wordstr);
-                }
-            }
-            node = retNode;
-        } else {
-            return false;
-        }
-    }
-
-    if (retVector.size()) {
-        return true;
-    }
-
-    return false;
-}
-
-inline auto add_word_to_tree(string word, Node * node) -> void
-{
-
     if (node == nullptr) {
         return;
     }
 
-    Node * retNode{nullptr};
-    Node * lastNode{nullptr};
-    size_t len = word.length();
+    if (word.length() == 0) {
+        return;
+    }
+
+    string wordstr;
+    retNode = node;
+    retVector.clear();
+
+    for (auto & letter : word) {
+        bool retval = find_char_node (letter, node, retNode);
+        if (retval) {
+            wordstr += letter;
+            if (retNode->EOW || wordstr.length() == word.length() - 1) {
+                retVector.emplace_back(wordstr);
+            }
+            node = retNode;
+        } else {
+            return;
+        }
+    }
+
+//    if (retVector.size()) {
+//        return;
+//    }
+//
+    return;
+}
+
+inline auto add_word_to_tree(string word, NodePtr node) -> void
+{
+    if (node == nullptr) {
+        return;
+    }
+
+    NodePtr retNode {nullptr};
+    NodePtr lastNode {nullptr};
     string temp_str;
-    for (auto iter : word) {
+    for (auto & letter : word) {
         if (node != nullptr) {
-            retNode = nullptr;
-            bool retval = find_char_node (iter, node, retNode);
+            bool retval = find_char_node (letter, node, retNode);
             if (retval == false) {
-                lastNode = newNode(iter);
+                lastNode = newNode(letter);
                 lastNode->parent = node;                // new node's parent
                 (node->child).emplace_back(lastNode);   // add word
-                retNode = nullptr;
-                retval = find_char_node (iter, node, retNode);
+                retval = find_char_node (letter, node, retNode);        // confirm new letter is there
                 if (retval == true) {
                     node = retNode;
                 }
             } else {
                 temp_str += retNode->key;
-                if (temp_str.length() == len) {
+                if (temp_str.length() == word.length()) {
                     if (!retNode->EOW) {
                         retNode->EOW = true;    // since adding word within another word
                     }
@@ -202,21 +180,24 @@ class DFS {
 public:
     constexpr DFS () {}
 
-    inline auto pre(const Node * node, IntVector * ans) -> const void {
-        ans->emplace_back(node->key);
-        for (Node * child : node->child) {
-            pre(child, ans);
-        }
-    }
+//    inline auto pre(const Node * node, IntVector * ans) -> const void {
+//        ans->emplace_back(node->key);
+//        for (Node * child : node->child) {
+//            pre(child, ans);
+//        }
+//    }
 
-    inline constexpr auto findDepthOfTree(const struct Node *node) -> int
+    //inline constexpr auto findDepthOfTree(const struct Node *node) -> int
+    inline auto findDepthOfTree(const NodePtr node) -> int
     {
         if (node == NULL) {
             return 0;
         }
-        int maxDepth{0};
-        for (vector<Node*>::const_iterator c_it = cbegin(node->child); c_it != cend(node->child); ++c_it) {
-            maxDepth = max(maxDepth, findDepthOfTree(*c_it));
+        int maxDepth {0};
+        //for (vector<Node*>::const_iterator c_it = cbegin(node->child); c_it != cend(node->child); ++c_it) {
+        for (auto & it : node->child) {
+            //maxDepth = max(maxDepth, findDepthOfTree(*c_it));
+            maxDepth = max(maxDepth, findDepthOfTree(it));
         }
         return maxDepth + 1;
     }
@@ -240,13 +221,10 @@ int main()
 
     // fill vector with the lines from file
 
-    if (myfile.is_open())
-    {
-        while ( getline (myfile, line) )
-        {
+    if (myfile.is_open()) {
+        while (getline (myfile, line)) {
             line.erase(line.find_last_not_of(" \n\r\t")+1); // trim white space
-	    if (line != "")
-	    {
+	    if (line != "") {
 	        v_the_list.emplace_back(line);
                 total_lines_read++;
     	    }
@@ -255,12 +233,11 @@ int main()
     } else {
         cout << "myfile NOT open" << endl;
     }
-//    queue <char> q;
 
     root = newNode('/');
     setRoot(root);
 
-    for (auto word : v_the_list) {
+    for (auto & word : v_the_list) {
         add_word_to_tree(word, root);
     }
     cout << endl;
@@ -299,10 +276,9 @@ int main()
 //word = "ethylenediaminetetraacetate";
 //test_list.emplace_back(word);
 //    for (auto word : test_list)
-    for (auto word : v_the_list) 
+    for (auto & word : v_the_list) 
     {
         StringVector retVector;
-        StringVector tempvector;
         StringVector listOfWords;
 
         listOfWords.emplace_back(word);
@@ -311,55 +287,48 @@ int main()
         string lastRemainder;
 
         while (listOfWords.size()) {
-            for (auto list_word : listOfWords) {
+            StringVector tempvector;
+            for (auto & list_word : listOfWords) {
                 // get all words in compound word
-                //bool retval = isCompoundWord(list_word, getRoot(), retNode, retVector); 
                 isCompoundWord(list_word, getRoot(), retNode, retVector); 
                 // make sure its a unique string entry vector
                 if (retVector.size()) {
-                    for (auto prefix : retVector) {
-                        //string remainder = list_word.substr(prefix.length(), -1);
-                        string remainder(move(list_word.substr(prefix.length(), -1)));
+                    for (auto & prefix : retVector) {
+                        string remainder = list_word.substr(prefix.length(), -1);
                         if (prefix.length() < 2 || remainder.length() < 2) continue;
-                        if (remainder.length() >= 2) {
-                            //wordMap.emplace(prefix, remainder);
-                            auto it = wordMap.end();
-                            it = wordMap.emplace_hint(it, prefix,remainder);
-                            bool retval = isWord (remainder, getRoot(), retNode);
-                            tempvector.emplace_back(remainder);
-                            if (retval == true) {
-                                // found compound word
-                                lastPrefix = move(prefix);
-                                lastRemainder = move(remainder);
-                                break;
-                            }
+                        auto it = end(wordMap);
+                        it = wordMap.emplace_hint(it, prefix, remainder);
+                        bool retval = isWord (remainder, getRoot(), retNode);
+                        tempvector.emplace_back(remainder);
+                        if (retval == true) {
+                            // found compound word
+                            lastPrefix = prefix;
+                            lastRemainder = remainder;
+                            break;
                         }
                     }
                 }
             }
-            listOfWords = move(tempvector);
-            tempvector.clear();
+            listOfWords = tempvector;
         }
-
-        string wordFromList(move(word));
+        string wordFromList(word);
 
 restart: 
-        for (auto m_word : wordMap) {
+        for (auto & m_word : wordMap) {
             if ((m_word.first.length() < 2) || (m_word.second.length() < 2)) continue;
-            if (lastRemainder.compare(m_word.second) == 0) {
-                lastRemainder = m_word.first + m_word.second;   // next Remainder
-                if (wordFromList.compare(lastRemainder) == 0) {
-//                cout << "MATCH" << endl;
-                    matchVector.emplace_back(wordFromList);
+            if (lastRemainder.compare(m_word.second) != 0) continue;
+            lastRemainder = m_word.first + m_word.second;   // next Remainder
+            if (wordFromList.compare(lastRemainder) == 0) {
+                //cout << "MATCH" << endl;
+                matchVector.emplace_back(wordFromList);
 
-                    auto it = wordMap.find(m_word.first);
-                    if (it != end(wordMap)) {
-                        wordMap.erase(it);
-                    }
-                    break;
+                auto it = wordMap.find(m_word.first);
+                if (it != end(wordMap)) {
+                    wordMap.erase(it);
                 }
-                goto restart;
-           }
+                break;
+            }
+            goto restart;
         }
     }
 
@@ -368,7 +337,7 @@ restart:
     cout << "Compound words found: " << matchVector.size() << endl;
     size_t matchCnt {0};
 
-    for (auto w : matchVector) {
+    for (auto & w : matchVector) {
         cout << "\t[" << w << "]" << endl;
         matchCnt++;
         if (matchCnt >= 10) break;
